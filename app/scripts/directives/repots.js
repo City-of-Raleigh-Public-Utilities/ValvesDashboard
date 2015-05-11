9201359b387b2f7518ca4256af7ec2f0bdc1052f
cwhite911 <smortopahri@gmail.com>
2015-05-11 14:29:39 -0400

    Added new report directive

---
+++ app/scripts/directives/repots.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name valvessDashboard.directive:repots
+ * @description
+ * # repots
+ */
+angular.module('valvesDashboard')
+  .directive('repots', function () {
+    return {
+      templateUrl: 'views/reports.html',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the repots directive');
+      }
+    };
+  });
---
+++ test/spec/directives/repots.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Directive: repots', function () {
+
+  // load the directive's module
+  beforeEach(module('valvesDashboard'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<repots></repots>');
+    element = $compile(element)(scope);
+    expect(element.text()).toBe('this is the repots directive');
+  }));
+});
